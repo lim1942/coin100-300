c9d6d9959313480bf1bf7052bc460f98715df7f4
bigblock <bigblock@louisdeiMac.local>
2018-11-13 18:07:06 +0800

    add

---
+++ kyber-network.py
@@ -0,0 +1,90 @@
+import os
+import json
+import time
+import requests
+import traceback
+from lxml import etree
+
+from tools import my_mq, my_format ,json_download, html_download, my_websocket
+from tools import DepthItem ,TickerItem ,TradeItem, Symbols ,SOCK_PROXIES, HEADERS
+
+file_name = os.path.basename(__file__).split('.')[0]
+DepthItem = DepthItem + '_' + file_name
+TickerItem = TickerItem + '_' + file_name
+TradeItem = TradeItem + '_' + file_name
+Symbols = Symbols + '_' + file_name
+
+rabbitmq_url = 'amqp://guest:123456@127.0.0.1:5672'
+
+
+
+def parse(exchange_id,exchange_name=file_name):
+
+    my_format_obj = my_format()
+    symbols_mq = my_mq(Symbols, Symbols, Symbols,rabbitmq_url=rabbitmq_url)
+    tickers_mq = my_mq(TickerItem, TickerItem,TickerItem,rabbitmq_url=rabbitmq_url)
+
+
+    def get_symbols():
+        map_dict = dict()
+        # 1
+        url = 'https://production-cache.kyber.network/getRightMarketInfo'
+        res = json_download(url)
+        # 2
+        res = res['data'].items()
+        symbols = []
+        for k,v in res:
+            #4
+            subject = k+'^ETH'
+            symbols.append(subject)
+        symbols_message = my_format_obj.format_symbols(exchange_id, symbols, exchange_name)
+        symbols_mq.send_message(symbols_message)
+        print(symbols_message)
+        return map_dict
+
+
+    def get_tickers():
+        # 1
+        url = 'https://production-cache.kyber.network/getRightMarketInfo'
+        res = json_download(url)
+        # 2
+        res = res['data'].items()
+        ts = my_format_obj.get_13_str_time()
+        for k,v in res:
+            #3
+            subject = k+'^ETH'
+            #4
+            try:
+                price = v['rate']
+            except:
+                continue
+            if not price:
+                continue
+            # ts = my_format_obj.get_13_str_time(i[])
+            unit = my_format_obj.get_unit(price)
+            ticker_message = my_format_obj.format_tick(exchange_name, subject, exchange_id, price, unit, ts)
+            tickers_mq.send_message(ticker_message)
+            print(ticker_message)
+
+
+    while 1:
+        try:    
+            map_dict = get_symbols()
+            while  1:
+                try:
+                    get_tickers()
+                except Exception as e:
+                    print('eid:',exchange_id,traceback.print_exc())
+                time.sleep(1)
+
+        except Exception as e:
+            print('eid:', exchange_id, traceback.print_exc())
+        time.sleep(1)
+
+
+
+if __name__ == '__main__':
+    print(file_name,'\n')
+    #5
+    exchange_id = '217'
+    parse(exchange_id)
---
+++ neraexpro.py
@@ -0,0 +1,92 @@
+import os
+import json
+import time
+import requests
+import traceback
+from lxml import etree
+
+from tools import my_mq, my_format ,json_download, html_download, my_websocket
+from tools import DepthItem ,TickerItem ,TradeItem, Symbols ,SOCK_PROXIES, HEADERS
+
+file_name = os.path.basename(__file__).split('.')[0]
+DepthItem = DepthItem + '_' + file_name
+TickerItem = TickerItem + '_' + file_name
+TradeItem = TradeItem + '_' + file_name
+Symbols = Symbols + '_' + file_name
+
+rabbitmq_url = 'amqp://guest:123456@127.0.0.1:5672'
+
+
+
+def parse(exchange_id,exchange_name=file_name):
+
+    my_format_obj = my_format()
+    symbols_mq = my_mq(Symbols, Symbols, Symbols,rabbitmq_url=rabbitmq_url)
+    tickers_mq = my_mq(TickerItem, TickerItem,TickerItem,rabbitmq_url=rabbitmq_url)
+
+
+    def get_symbols():
+        map_dict = dict()
+        url = 'https://neraex.pro/api/v2/tickers'
+        res = json_download(url)
+        res = res.items()
+        symbols = []
+        for k,v in res:
+            subject = k
+            if subject.endswith('jpy'):
+                subject = subject.replace('jpy','^jpy')
+            elif subject.endswith('btc'):
+                subject = subject.replace('btc','^btc')               
+            else:
+                subject = subject[:3] + '^' + subject[3:]
+            subject = subject.upper()
+            symbols.append(subject)
+        symbols_message = my_format_obj.format_symbols(exchange_id, symbols, exchange_name)
+        symbols_mq.send_message(symbols_message)
+        print(symbols_message)
+        return map_dict
+
+
+    def get_tickers():
+        url = 'https://neraex.pro/api/v2/tickers'
+        res = json_download(url)
+        res = res.items()
+        ts = my_format_obj.get_13_str_time()
+        for k,v in res:
+            subject = k
+            if subject.endswith('jpy'):
+                subject = subject.replace('jpy','^jpy')
+            if subject.endswith('btc'):
+                subject = subject.replace('btc','^btc')               
+            else:
+                subject = subject[:3] + '^' + subject[3:]
+            subject = subject.upper()
+            price = v['ticker']['last']
+            ts = my_format_obj.get_13_str_time(v['at'])
+            unit = my_format_obj.get_unit(price)
+            ticker_message = my_format_obj.format_tick(exchange_name, subject, exchange_id, price, unit, ts)
+            tickers_mq.send_message(ticker_message)
+            print(ticker_message)
+
+
+    while 1:
+        try:    
+            map_dict = get_symbols()
+            while  1:
+                try:
+                    get_tickers()
+                except Exception as e:
+                    print('eid:',exchange_id,traceback.print_exc())
+                time.sleep(1)
+
+        except Exception as e:
+            print('eid:', exchange_id, traceback.print_exc())
+        time.sleep(1)
+
+
+
+if __name__ == '__main__':
+    print(file_name,'\n')
+    #5
+    exchange_id = '211'
+    parse(exchange_id)
